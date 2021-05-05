de4b9b87548522236a8238dd8ed215c79d8f9047
Jerem <jLFgtHb2103>
2021-05-05 16:30:53 +0200

    commit

--- multi_tools/errors/__init__.py
+++ multi_tools/errors/__init__.py
@@ -1,15 +1,1 @@
-from multi_tools.errors import fexception
-import traceback
-import sys
 
-
-def raise_f(err: fexception.FException):
-    err.__raise__()
-
-class FDefaultError:
-    def __init__(self, text):
-        self.text = text
-
-    def __raise__(self):
-
-        traceback.print_exc(file=sys.stderr)
